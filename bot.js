fd529d03a0738e2b2e8a161b4f7cd9d78c9e0644
randycod <45352823+randycod@users.noreply.github.com>
2018-11-27 20:50:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,37 +220,37 @@ client.on('message', message => {
   if (message.author.id !== "399353508429824000") return;
 
   
-  if (message.content.startsWith(prefix + 'setwat')) {
+  if (message.content.startsWith(prefix + 'sw')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('watching be' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}**`)
 } 
 
  
-  if (message.content.startsWith(prefix + 'setlis')) {
+  if (message.content.startsWith(prefix + 'sl')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('LISTENING be' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
 } 
 
 
-if (message.content.startsWith(prefix + 'setname')) {
+if (message.content.startsWith(prefix + 'sn')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
-if (message.content.startsWith(prefix + 'setavatar')) {
+if (message.content.startsWith(prefix + 'sa')) {
   client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'setstream')) {
+if (message.content.startsWith(prefix + 'ss')) {
   client.user.setGame(argresult, "https://www.twitch.tv/PlatinumBot");
      console.log('Streaming be' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}**`)
 } 
-if (message.content.startsWith(prefix + 'setplay')) {
+if (message.content.startsWith(prefix + 'sp')) {
   client.user.setGame(argresult);
      console.log('Playing be' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}**`)
